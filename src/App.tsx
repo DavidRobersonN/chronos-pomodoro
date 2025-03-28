128a819f2541d908f6e5a358682a72844c9778c0
David Roberson <david.roberson2010@gmail.com>
2025-03-28 19:59:20 -0300

    Iniciando componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
 export function App() {
   return (
     <>
       <Container>
+        <Logo />
+      </Container>
+
+      <Container>
         <Heading>LOGO</Heading>
-      </Container>
-
-      <Container>
-        <section>MENU</section>
-      </Container>
-
-      <Container>
-        <section>TEXT</section>
-      </Container>
-
-      <Container>
-        <section>FOOTER</section>
       </Container>
     </>
   );
